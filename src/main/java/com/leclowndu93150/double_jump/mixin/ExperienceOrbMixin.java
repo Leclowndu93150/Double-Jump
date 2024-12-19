672812ba23180c340312d13603ab958f9b35d645
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-12-19 09:16:24 +0100

    use a better approach to not break mending

--- src/main/java/com/leclowndu93150/double_jump/mixin/ExperienceOrbMixin.java
+++ src/main/java/com/leclowndu93150/double_jump/mixin/ExperienceOrbMixin.java
@@ -3,37 +3,32 @@ package com.leclowndu93150.double_jump.mixin;
 import com.leclowndu93150.double_jump.ExperienceConfig;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.ExperienceOrb;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ExperienceOrb.class)
 public abstract class ExperienceOrbMixin {
+    @Shadow public abstract int getValue();
 
-    @Inject(method = "playerTouch", at = @At("HEAD"), cancellable = true)
-    private void modifyExperience(Player player, CallbackInfo ci) {
-        if (!(player.level() instanceof ServerLevel serverLevel)) {
-            return;
+    @ModifyArg(
+            method = "playerTouch",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;giveExperiencePoints(I)V"
+            )
+    )
+    private int modifyExperienceValue(int value) {
+        ExperienceOrb orb = (ExperienceOrb) (Object) this;
+
+        if (!(orb.level() instanceof ServerLevel serverLevel)) {
+            return value;
         }
 
         String dimensionKey = serverLevel.dimension().location().toString();
-
         double multiplier = ExperienceConfig.load().getMultiplier(dimensionKey);
 
-        ExperienceOrb orb = (ExperienceOrb) (Object) this;
-        int originalValue = orb.getValue();
-
-        int modifiedValue = (int) (originalValue * multiplier);
-
-        player.giveExperiencePoints(modifiedValue);
-
-        orb.count--;
-        if (orb.count <= 0) {
-            orb.discard();
-        }
-
-        ci.cancel();
+        return (int) (value * multiplier);
     }
 }
